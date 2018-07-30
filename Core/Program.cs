d45822f5a9cdefffb234bd8aa0113f00cdc5e2bd
NATICK-NT\jbezuk <james.bezuk@cognex.com>
2018-07-30 14:13:35 -0400

    Added comments

--- Core/Program.cs
+++ Core/Program.cs
@@ -1,10 +1,12 @@
 public class Program
 {
+    /// Constructor
     public Program()
     {
         // Initialize
     }
 
+    /// Run method
     public void Run(params object[] parameters)
     {
         // Do some stuff
